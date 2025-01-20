d3cc6c2952e012398c6bc2d00534918cd7bf29b6
Samarth <77608432+Sam-dot-maker@users.noreply.github.com>
2025-01-20 14:03:06 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas
 
 st.title('My Parents New Healthy Diner')
 st.write('Choose the fruits you want in your custom Smoothie!')
