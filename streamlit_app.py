f8be71eac9339dbc2354d7e745dded3b2337c33f
Samarth <77608432+Sam-dot-maker@users.noreply.github.com>
2025-01-20 15:47:03 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ st.write('The name on your Smoothie will be:', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table('Smoothies.public.fruit_options').select(col('fruit_name'), col('search_on')).collect()
-#st.dataframe(data = my_dataframe, use_container_width=True)
+st.dataframe(data = my_dataframe, use_container_width=True)
 #st.stop()
 
 # Convert the list of Row objects to a Pandas DataFrame
@@ -38,6 +38,7 @@ if ingredients_list:
     st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order) values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
     # st.write(my_insert_stmt)
+    
     
     time_to_insert = st.button('Submit Order')
 
